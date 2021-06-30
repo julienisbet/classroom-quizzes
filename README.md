8c1d2b00c686f78bf8e9cba6bd30ead19171855d
Julie Nisbet <jmnisbet@gmail.com>
2021-06-29 17:12:39 -0700

    adding logical operators quiz

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# logical-operators-warm-up
-Logical operators warm up
+# Logical Operators Quiz
+Idea borrowed from codequizzes.com
+For each logical operator, guess whether it will return true or false -- click on the box on the right to reveal the solution
+
---
+++ src/app.js
@@ -0,0 +1,55 @@
+const questions = [
+    `true && false`,
+    `true && true`,
+    `true || false`,
+    `!true`,
+    `!false`,
+    `!(true || false)`,
+    `1==="1"`,
+    `let a = 5;
+let b = 10;
+a >= 5 && b < 20;`,
+    `let a = 5;
+let b = 10;
+a === 5 || b === 20
+`,
+    `let a = 5;
+let b = 10;
+a > 10 || b > 10
+`,
+    `let a = 5;
+let b = 10;
+!(a > 10 || b > 10)`
+];
+const table = document.querySelector('table');
+questions.forEach(jsCode =>{
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    td2.classList.add('answer');
+    const pre = document.createElement('pre');
+    const code = document.createElement('code');
+    code.textContent = jsCode;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    td1.appendChild(pre);
+    pre.appendChild(code);
+    table.appendChild(tr);
+});
+
+const answers = document.querySelectorAll('.answer');
+const getAnswer = function(e){
+    
+    if (this.classList.contains('revealed')){
+        this.innerText = '';
+        this.classList.remove('revealed');
+    } else {
+        this.classList.add('revealed');
+        const val = this.parentElement.querySelector('code').innerText;
+        const answer = eval(val);
+        e.target.innerHTML = `<pre><code class="language-javascript">${answer}</code></pre>`;
+    }
+};
+answers.forEach(e =>{
+    e.addEventListener('click', getAnswer);
+});
